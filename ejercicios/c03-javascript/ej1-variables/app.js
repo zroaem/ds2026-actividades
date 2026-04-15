669602b485c4abec0d0b20fe601525af17385c6c
Zoe Ramljak <ramljakzoe@gmail.com>
2026-04-14 21:08:39 -0300

    Ejercicio 1: variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,13 @@
+const nombre = "Zoe";
+const edad = 20;
+const materia = "Desarrollo De Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+
+for(let i=0; i<3; i++){
+    contador++;
+}
+
+console.log(`Valor final contador: ${contador}`);
