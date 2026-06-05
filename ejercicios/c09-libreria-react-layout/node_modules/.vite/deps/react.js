3e7080537b74deb5d65ae3fde8c2964ed3507742
Zoe Ramljak <ramljakzoe@gmail.com>
2026-06-04 21:22:57 -0300

    Arregle el .gitignore y saque los node_modules del tracking

--- ejercicios/c09-libreria-react-layout/node_modules/.vite/deps/react.js
+++
@@ -1,2 +0,0 @@
-import { t as require_react } from "./react-CoTh1R2n.js";
-export default require_react();
--- ejercicios/c09-libreria-react-layout/node_modules/rolldown/dist/config.mjs
+++
@@ -1,4 +0,0 @@
-import { x as VERSION } from "./shared/bindingify-input-options-DmY7myA4.mjs";
-import { t as defineConfig } from "./shared/define-config-Demdg3_4.mjs";
-import { t as loadConfig } from "./shared/load-config-ByfeVF0s.mjs";
-export { VERSION, defineConfig, loadConfig };
